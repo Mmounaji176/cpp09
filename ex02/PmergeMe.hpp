3c73724a2033f03c573513a09f9dc66f5ea0088d
mmounaji <92118864+Mmounaji176@users.noreply.github.com>
2023-09-10 17:13:12 +0000

    done

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -50,67 +50,100 @@ void printContainer(T &container)
 }
 
 template <typename T>
-void    insertionAlgo(T &container)
-{
-    typename T::iterator it = container.begin();
-    typename T::iterator itNext = container.begin();
-    itNext++;
-    while (itNext != container.end())
-    {
-        if (*it > *itNext)
-        {
-            typename T::iterator itPrev = it;
-            itPrev--;
-            while (itPrev != container.begin() && *itPrev > *itNext)
-                itPrev--;
-            itPrev++;
-            container.insert(itPrev, *itNext);
-            itNext = container.erase(itNext);
+void insertionSort(std::vector<T>& arr, int left, int right) {
+    for (int i = left + 1; i <= right; ++i) {
+        T key = arr[i];
+        int j = i - 1;
+
+        while (j >= left && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
         }
-        else
-        {
-            it++;
-            itNext++;
+
+        arr[j + 1] = key;
+    }
+}
+
+template <typename T>
+void merge(std::vector<T>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    std::vector<T> leftArr(n1);
+    std::vector<T> rightArr(n2);
+
+    for (int i = 0; i < n1; i++) {
+        leftArr[i] = arr[left + i];
+    }
+
+    for (int j = 0; j < n2; j++) {
+        rightArr[j] = arr[mid + 1 + j];
+    }
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (leftArr[i] <= rightArr[j]) {
+            arr[k] = leftArr[i];
+            i++;
+        } else {
+            arr[k] = rightArr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = leftArr[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = rightArr[j];
+        j++;
+        k++;
+    }
+}
+
+template <typename T>
+void mergeInsertionSort(std::vector<T>& arr, int left, int right, int insertion_threshold) {
+    if (left < right) {
+        if (right - left + 1 <= insertion_threshold) {
+            insertionSort(arr, left, right);
+        } else {
+            int mid = left + (right - left) / 2;
+            mergeInsertionSort(arr, left, mid, insertion_threshold);
+            mergeInsertionSort(arr, mid + 1, right, insertion_threshold);
+            merge(arr, left, mid, right);
         }
     }
 }
 
 template <typename T>
-void    merge(T &container, T &left, T &right)
-{
-    typename T::iterator it = container.begin();
-    typename T::iterator itLeft = left.begin();
-    typename T::iterator itRight = right.begin();
-    while (itLeft != left.end() && itRight != right.end())
-    {
-        if (*itLeft < *itRight)
-            *it++ = *itLeft++;
-        else
-            *it++ = *itRight++;
+void mergeInsertionSort(std::vector<T>& arr, int insertion_threshold = 10) {
+    int n = arr.size();
+    mergeInsertionSort(arr, 0, n - 1, insertion_threshold);
+}
+
+int main() {
+    std::vector<int> arr = {12, 11, 13, 5, 6, 7};
+    int n = arr.size();
+
+    std::cout << "Original Array: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
     }
-    while (itLeft != left.end())
-        *it++ = *itLeft++;
-    while (itRight != right.end())
-        *it++ = *itRight++;
-}
+    std::cout << std::endl;
 
-template <typename T>
-void mergeInsertionSort(T &container)
-{
-    int size = container.size();
-    if (size == 1)
-        return;
-    int middle = size / 2;
-    T left;
-    T right;
-    typename T::iterator it = container.begin();
-    for (int i = 0; i < middle; i++)
-        left.push_back(*it++);
-    for (int i = middle; i < size; i++)
-        right.push_back(*it++);
-    mergeInsertionSort(left);
-    mergeInsertionSort(right);
-    merge(container, left, right);
-}
+    mergeInsertionSort(arr);
 
+    std::cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
 #endif
